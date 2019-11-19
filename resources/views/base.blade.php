99fe92db357a43f6c12a91b22272f10ba567b6c9
hani-mansour <hani.mansour.1990@gmail.com>
2019-11-19 14:56:19 +0100

    sorte table is done

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -7,6 +7,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <!-- Styles -->
       <link href="/css/app.css" rel="stylesheet">
--- resources/views/patients.blade.php
+++ resources/views/patients.blade.php
@@ -1,28 +1,46 @@
 @extends('backend')
 
 @section('title')Patienten &ndash;
-@parent
+@parent  {{--der Title die von Eltern('base') erzeugen--}}
 @endsection
 
 @section('main')
-    <table class="table">
-        <tr>
-            <th>Svrn</th>
-            <th>Name</th>
-            <th>Adresse</th>
-            <th>PLZ</th>
-            <th>Ort</th>
-            <th>Land</th>
-        </tr>
-    @foreach($patients as $patient)
-        <tr>
-            <td><p>{{$patient->svnr}}</p></td>
-            <td><p>{{$patient->lastname}},{{$patient->firstname}}</p></td>
-            <td><p>{{$patient->address}}</p></td>
-            <td><p>{{$patient->plz}}</p></td>
-            <td><p>{{$patient->city}}</p></td>
-            <td><p>{{$patient->country}}</p></td>
-        </tr>
-    @endforeach
-    </table>
+    <div class="container">
+        <form method="post" action="{{route('authenticate')}}">
+            @csrf
+            @if( $errors->any() )
+                <p class="alert alert-danger">Nice try.</p>
+            @endif
+            <p><input type="text" name="name" placeholder="Patient Name"></p>
+            <p><button type="submit">Filter</button></p>
+        </form>
+
+        <table class="table table-bordered">
+            <tr>
+                <th>@sortablelink('svnr')</th>
+                <th>@sortablelink('firstname')</th>
+                <th>@sortablelink('lastname')</th>
+                <th>@sortablelink('email')</th>
+                <th>@sortablelink('address')</th>
+                <th>@sortablelink('plz ')</th>
+                <th>@sortablelink('city')</th>
+                <th>@sortablelink('country')</th>
+            </tr>
+            @if($patients->count())
+                @foreach($patients as $patient)
+                    <tr>
+                        <td>{{ $patient->svnr }}</td>
+                        <td>{{ $patient->firstname }}</td>
+                        <td>{{ $patient->lastname }}</td>
+                        <td>{{ $patient->email }}</td>
+                        <td>{{ $patient->address }}</td>
+                        <td>{{ $patient->plz }}</td>
+                        <td>{{ $patient->city }}</td>
+                        <td>{{ $patient->country }}</td>
+                    </tr>
+                @endforeach
+            @endif
+        </table>
+        {!! $patients->appends(\Request::except('page'))->render() !!}
+    </div>
 @endsection
