2ddbece7ed95a0bc4cc9936c3d59452fef6c67f5
hani-mansour <hani.mansour.1990@gmail.com>
2019-11-20 16:04:52 +0100

    added files

---
+++ app/Patient.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Kyslik\ColumnSortable\Sortable;
+
+class Patient extends Model
+{
+    use Sortable;
+
+
+    protected $fillable = [ 'svnr', 'firstname', 'lastname', 'email', 'address', 'plz', 'city', 'country' ];
+
+
+    public $sortable = ['id', 'name', 'firstname', 'lastname', 'email', 'address', 'plz', 'city', 'country', 'created_at', 'updated_at'];
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}
